f91cac80827cf91f17ed4236391196e95b645a7c
vaveyko <zakhvey.ivan@gmail.com>
2026-02-15 16:48:06 +0300

    return to request library for getting site html

--- PreprocessText.py
+++ PreprocessText.py
@@ -1,17 +1,15 @@
 import time
 
-from selenium import webdriver
-from selenium.common import WebDriverException
-from selenium.webdriver.chrome.options import Options
+import requests
 from bs4 import BeautifulSoup
 from typing import List
 
 
 class URLParser():
     def __init__(self):
-        options = Options()
-        options.add_argument("--headless")  # без окна браузера
-        self.driver = webdriver.Edge(options=options)
+        self.headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+        }
 
     def parse(self, urls: List[str,]) -> List[List[str,]]:
         return [self.parse_one(url) for url in urls]
@@ -19,23 +17,22 @@ class URLParser():
     def parse_one(self, url: str) -> List[str,]:
         print("Request to site;", end=" ")
         try:
-            self.driver.get(url)
-        except WebDriverException as e:
+            response = requests.get(url, headers=self.headers, timeout=10)
+
+            print("Get html;", end=" ")
+            soup = BeautifulSoup(response.text, 'html.parser')
+
+        except Exception as e:
             print("URL is not available, error")
             return []
-        print("Waiting;", end=" ")
-        time.sleep(3)
-        print("Get html;", end=" ")
-        html = self.driver.page_source
-        soup = BeautifulSoup(html, "lxml")
 
         print("Parse html;")
         tags = soup.find_all(['h1', 'h2', 'h3', 'h4'])
         links_1 = soup.find_all("a", class_=lambda x: x and ("product" in x))
         links_2 = soup.find_all("a", href=lambda h: h and "product" in h)
 
-        link_text = [link.get_text(strip=True) for link in links_1] + [link.get_text(strip=True) for link in links_2]
-        text = [tag.get_text(strip=True) for tag in tags]
+        link_text = [link.get_text(strip=True, separator=" ") for link in links_1] + [link.get_text(strip=True) for link in links_2]
+        text = [tag.get_text(strip=True, separator=" ") for tag in tags]
 
         preprocessed_data = []
         for line in link_text + text:
